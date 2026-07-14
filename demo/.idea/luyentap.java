2c482be43cadecca12910f8498138b6005d22725
Nguyen Trung Kien <kiennguyen24062006@gmail.com>
2026-07-14 18:46:14 +0700

    bai tap luyen tay

--- demo/.idea/luyentap.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int tuoi = 19;
-        if (tuoi >= 19){
-            System.out.println("ban du tuoi");}
-        else {
-            System.out.println(" bn chua du tuoi ");
-        }
-    }
